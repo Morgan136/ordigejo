c11ec74fc58bcda482121917ec71557be8320297
Morgan136 <rbnkv.dnl@gmail.com>
2024-12-08 17:45:03 +0500

    implementation of element's card

--- karto.py
+++ karto.py
@@ -1,51 +1,77 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QLabel, QLineEdit, QVBoxLayout
-import time
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QWidget, QPushButton
+import sys
 from PyQt6.QtCore import Qt
-import random
 
-from qwt.legend import buttonShift
-
-list1 = [
-    "dsadad10",
-    "dsadad9",
-    "dsadad8",
-    "dsadad7",
-    "dsadad6",
-    "dsadad5",
-    "dsadad4",
-    "dsadad3",
-    "dsadad2",
-    "dsadad1"
+sortedList = [
+    ["defaultName", "defaultDate", "defaultAux"],
+    ["name1", "date1", "aux1"],
+    ["name2", "date2", "aux2"],
+    ["name3", "date3", "aux3"],
+    ["name4", "date4", "aux4"]
 ]
 
-
 class MainWindow(QMainWindow):
+
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("My App")
+        self.setWindowTitle("Ordigejo")
 
-        self.label = QLabel(random.choice(list1))
-        self.button = QPushButton("b for text")
-        self.input = QLineEdit()
+        self.n = 0
 
-        self.input.textChanged.connect(self.button.setText)
-        self.button.clicked.connect(self.label_change)
+        self.elementName = QLabel(sortedList[0][0])
+        self.elementDate = QLabel(sortedList[0][1])
+        self.elementAux = QLabel(sortedList[0][2])
+
+        self.nextButton = QPushButton("Next")
+        self.nextButton.clicked.connect(self.nextElement)
+
+        self.previousButton = QPushButton("Previous")
+        self.previousButton.clicked.connect(self.previousElement)
 
         layout = QVBoxLayout()
-        layout.addWidget(self.input)
-        layout.addWidget(self.label)
-        layout.addWidget(self.button, alignment = Qt.AlignmentFlag.AlignRight)
+        layout.addWidget(self.elementName)
+        layout.addWidget(self.elementDate)
+        layout.addWidget(self.elementAux)
+        layout.addWidget(self.nextButton, alignment=Qt.AlignmentFlag.AlignRight)
+        layout.addWidget(self.previousButton, alignment=Qt.AlignmentFlag.AlignLeft)
+
+
 
         container = QWidget()
         container.setLayout(layout)
 
         self.setCentralWidget(container)
 
-    def label_change(self):
-        self.label.setText(random.choice(list1))
+    def nextElement(self):
+        self.n += 1
 
-app = QApplication([])
+        try:
+            self.elementName.setText(sortedList[self.n][0])
+            self.elementDate.setText(sortedList[self.n][1])
+            self.elementAux.setText(sortedList[self.n][2])
+        except IndexError:
+            self.n = 0
+            self.elementName.setText(sortedList[self.n][0])
+            self.elementDate.setText(sortedList[self.n][1])
+            self.elementAux.setText(sortedList[self.n][2])
+
+
+    def previousElement(self):
+        self.n -= 1
+
+        try:
+            self.elementName.setText(sortedList[self.n][0])
+            self.elementDate.setText(sortedList[self.n][1])
+            self.elementAux.setText(sortedList[self.n][2])
+        except IndexError:
+            self.n = len(sortedList) - 1
+            self.elementName.setText(sortedList[self.n][0])
+            self.elementDate.setText(sortedList[self.n][1])
+            self.elementAux.setText(sortedList[self.n][2])
+
+
+app = QApplication(sys.argv)
 
 window = MainWindow()
 window.show()
