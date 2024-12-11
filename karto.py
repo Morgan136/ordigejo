54a6d9466b0a3bf6864e33b19c7e541a3e5c0fbd
Morgan136 <rbnkv.dnl@gmail.com>
2024-12-11 20:46:23 +0500

    commit

--- karto.py
+++ karto.py
@@ -1,16 +1,18 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout, QWidget, \
-    QPushButton, QToolBar, QCheckBox, QStatusBar, QGridLayout
+    QPushButton, QToolBar, QCheckBox, QStatusBar, QGridLayout, QTextEdit
 from PyQt6.QtGui import QAction, QIcon
 import sys
 from PyQt6.QtCore import Qt, QSize, QRect
 import lorem
+from datetime import datetime
+import time
 
 sortedList = [
     ["default_Name", "de Date", "el Источник", "priskribo " * 100],
-    ["name1", "date1", "aux1", lorem.get_paragraph(5) + "КОНЕЦ КОНЕЦ КОНЕЦ"],
+    ["name1\nsecond string", "date1", "aux1", lorem.get_paragraph(5) + "КОНЕЦ КОНЕЦ КОНЕЦ"],
     ["name2", "date2", "aux2", "description2 " * 100],
-    ["name3", "date3", "aux3", "description3 " * 100],
-    ["name4", "date4", "aux4", "description4 " * 100],
+    ["name3", str(datetime(1900, 12, 25, 13, 13)), "aux3", "description3 " * 100],
+    ["name4", "de " + datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "aux4", "description4 " * 100],
 ]
 
 class MainWindow(QMainWindow):
@@ -19,7 +21,7 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.width = 600
-        self.height = 400
+        self.height = 500
 
         self.setFixedSize(self.width, self.height)
         self.setWindowTitle("Ordigejo")
@@ -32,16 +34,26 @@ class MainWindow(QMainWindow):
         toolbar.setIconSize(QSize(16, 16))
         self.addToolBar(toolbar)
 
-        #toolbar.addSeparator()
+        buttonImport = QAction(QIcon("bug.png"), "Импорт данных", self)
+        buttonExport = QAction(QIcon("bug.png"), "Экспорт данных", self)
+        buttonSource = QAction(QIcon("bug.png"), "Показать файл в Проводнике", self)
 
-        button_action = QAction(QIcon("bug.png"), "Кнопка 1", self)
-        button_action2 = QAction(QIcon("bug.png"), "Кнопка 2", self)
+        buttonPreferencesDesign = QAction(QIcon("bug.png"), "Дизайн", self)
+        buttonPreferencesImport = QAction(QIcon("bug.png"), "Импорт настроек", self)
+        buttonPreferencesExport = QAction(QIcon("bug.png"), "Экспорт настроек", self)
 
         menu = self.menuBar()
 
         file_menu = menu.addMenu("File")
-        file_menu.addAction(button_action)
-        file_menu.addAction(button_action2)
+        file_menu.addAction(buttonImport)
+        file_menu.addAction(buttonExport)
+        file_menu.addAction(buttonSource)
+
+        # подменю "Настройки"
+        menuPreferences = file_menu.addMenu(QIcon("bug.png"), "Настройки")
+        menuPreferences.addAction(buttonPreferencesDesign)
+        menuPreferences.addAction(buttonPreferencesImport)
+        menuPreferences.addAction(buttonPreferencesExport)
 
         self.setStatusBar(QStatusBar(self))
 
@@ -56,11 +68,9 @@ class MainWindow(QMainWindow):
         self.elementName = QLabel(sortedList[0][0], self)
 
         # Описание
-        self.elementDescription = QLabel(sortedList[0][3])
-        self.elementDescription.setStyleSheet('border-style: solid; border-width: 1px; border-color: black;')
+        self.elementDescription = QTextEdit(sortedList[0][3])
         self.elementDescription.setMaximumWidth(round(self.width * 0.75))
         self.elementDescription.setMaximumHeight(round(self.height * 1.85))
-        self.elementDescription.setWordWrap(True)
 
         # Дата
         self.elementDate = QLabel(sortedList[0][1])
@@ -76,10 +86,6 @@ class MainWindow(QMainWindow):
         self.nextButton.clicked.connect(self.nextElement)
         self.previousButton = QPushButton("Previous")
         self.previousButton.clicked.connect(self.previousElement)
-
-        #layoutMain.addLayout(layoutLeft) # добавление левой части: Имя, Описание
-        #layoutMain.addLayout(layoutRight) # добавление правой части: Дата, Aux
-        #layoutMain.addLayout(layoutControl) # добавление кнопок
 
         # добавление слоёв
         layoutMain = QGridLayout()
@@ -110,7 +116,6 @@ class MainWindow(QMainWindow):
 
     def nextElement(self):
         self.n += 1
-
         try:
             self.elementName.setText(sortedList[self.n][0])
             self.elementDate.setText(sortedList[self.n][1])
@@ -125,7 +130,6 @@ class MainWindow(QMainWindow):
 
     def previousElement(self):
         self.n -= 1
-
         try:
             self.elementName.setText(sortedList[self.n][0])
             self.elementDate.setText(sortedList[self.n][1])
@@ -137,6 +141,9 @@ class MainWindow(QMainWindow):
             self.elementDate.setText(sortedList[self.n][1])
             self.elementAux.setText(sortedList[self.n][2])
             self.elementDescription.setText(sortedList[self.n][3])
+
+        def mouseDoubleClickEvent(self, e):
+            self.label.setText("mouseDoubleClickEvent")
 
 app = QApplication(sys.argv)
 
